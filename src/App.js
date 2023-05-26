508ebb0ad179905149add4bc78907a2d2efb3459
Dsimmanuel <dsimmanuel592@gmail.com>
2023-05-26 07:15:30 +0530

    Homepage

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Banner from './Components/Banner/Banner';
+import Rowpost from './Components/Rowpost/Rowpost';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Banner />
+      <Rowpost />
+    </div>
+  );
+}
+
+export default App;
